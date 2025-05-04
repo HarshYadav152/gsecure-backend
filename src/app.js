724935341c1f01844a889093a5abeee74bbb39fa
Harsh Yadav <161936085+HarshYadav152@users.noreply.github.com>
2025-05-04 12:11:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // middlewares
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://gsecure-frontent.vercel.app",
     credentials: true
 }))
 app.use(express.json())
